bc4b19ec3e2b2bc984259cdaab55b022c1808c7a
Sam Cox <sjamcox@gmail.com>
2021-02-23 12:25:15 -0800

    Switch font source to CDN

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
 import Header from './components/Header';
 
 ReactDOM.render(<Header />, document.getElementById('header'));
